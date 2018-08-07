ac94e855134a9ea7475c6bc51f3b78e9375ed5a1
Adrian <adrian.sanchez@demosense.com>
2018-08-07 10:22:33 +0200

    fix: Wrong setup package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 if sys.version_info < (3, 6):
     sys.exit('Sorry, Python < 3.6 is not supported')
 
-setup(name='boto3-utils',
+setup(name='boto3_utils',
       version='1.0.0',
       description='Python package for some functionality related with AWS Boto3 SDK, specially useful in AWS Lambda.'
                   'The functionality does an atomic operation to avoid connexion interruptions, also downloads the '
